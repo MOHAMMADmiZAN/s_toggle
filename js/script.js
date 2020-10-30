05407fe7810885df13e1256f4842a1c8d6a1ac0a
MOHAMMAD MIZAN <mohammad25524143@gmail.com>
2020-10-30 23:14:40 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(".one").click(function () {
+    $(".chk1").fadeToggle();
+    $('.one').toggleClass('clr-bg');
+
+
+})
+
+
+$(".two").click(function () {
+    $(".chk2").fadeToggle();
+    $('.two').toggleClass('clr-bg');
+
+})
+$(".three").click(function () {
+    $(".chk3").fadeToggle();
+    $('.three').toggleClass('clr-bg');
+})
+$(".four").click(function () {
+    $(".chk4").fadeToggle();
+    $('.four').toggleClass('clr-bg');
+})
+$(".five").click(function () {
+    $(".chk5").fadeToggle();
+    $('.five').toggleClass('clr-bg');
+})
+$(".six").click(function () {
+    $(".chk6").fadeToggle();
+    $('.six').toggleClass('clr-bg');
+})
+
+
+
